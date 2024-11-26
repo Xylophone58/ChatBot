bcf1dbcbd89379aa11324facebd7e9d3878bb0e0
Zach-ar-y <ZDM69360@ucmo.edu>
2024-11-25 18:45:33 -0600

    update 2

--- ChatBot/obj/Debug/net8.0-windows/ChatBot.AssemblyInfo.cs
+++ ChatBot/obj/Debug/net8.0-windows/ChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbb9f00b31a33ec915625b66abaef483ddc7bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c516c5a530a4bedb3d5df11e96163d016f29c3e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
